e3b39eecee36224a58c3bcf5a43ad9cd1c27ce3b
christopherv2264 <91750550+christopherv2264@users.noreply.github.com>
2021-10-01 14:27:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let ownerName = "c. vergara"; // update the part BETWEEN the "quotes"
 let userName = "christopherv2264"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
+    e.innerHTML = christopherv2264;
 });
 
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
+document.querySelector("#github").href = "http://github.com/" + christopherv2264;
+document.querySelector("#fork").href = "http://github.com/" + christopherv2264 + "/" + christopher2264 + ".github.io";
